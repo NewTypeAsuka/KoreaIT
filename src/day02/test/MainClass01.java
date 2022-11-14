11fb53f7bf0ecde47bbeeeefafc035e72bd03df2
ITSC <ITSC@gmail.com>
2022-11-14 19:39:56 +0900

    첫 커밋

---
+++ src/day02/test/MainClass01.java
@@ -0,0 +1,20 @@
+package day02.test;
+/*
+ * 주석이란?
+ * 	컴퓨터가 코드로 인식하지 않는다.
+ */
+
+// 한줄 주석
+/*
+ * 여러 줄 주석
+ */
+
+public class MainClass01 {
+	// 프로그램 시작이 되는 메인메소드
+	public static void main(String[] args) {
+		System.out.println("메인메소드 시작!");
+		
+		
+	}
+	
+}
